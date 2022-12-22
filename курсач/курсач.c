226f85e2f12299566e651e1059a6fa3202c310bc
Sobaka <118391159+jopa-sobaki@users.noreply.github.com>
2022-12-23 01:19:19 +0300

    Update курсач.c

--- курсач/курсач.c
+++ курсач/курсач.c
@@ -12,10 +12,10 @@ void poisk();
 int main()
 {
 	float consts[5][2] = {
-		{157.0, 50.0}, {73.0, 100.0}, {30.0, 50.0}, {80.0, 100.0}, {63.0, 100.0}
+		{43.0, 50.0}, {73.0, 100.0}, {30.0, 50.0}, {80.0, 100.0}, {63.0, 100.0}
 	};
 	setlocale(LC_ALL, "RUS");
-	double arr[5][5];
+	double arr[5][4];
 	char  extension[6] = { ".txt\0" };
 	char file_name[40];
 	puts("Введите название файла в пределах 30 символов (без указания расширения файла)");
@@ -38,8 +38,7 @@ int main()
 	for (int i = 0; i < 5 ; i++) {
 		fscanf(Dokyment, "%lf ", &arr[i][0]);
 		fscanf(Dokyment, "%lf ", &arr[i][1]);
-		fscanf(Dokyment, "%lf ", &arr[i][2]);
-		fscanf(Dokyment, "%lf\n", &arr[i][3]);
+		fscanf(Dokyment, "%lf\n", &arr[i][2]);
 
 	}
 	
@@ -48,7 +47,7 @@ int main()
 	while (1>0)
 	{
 		int k;
-		printf("\n\nНажмите 1 чтобы показать таблицу, или 2 чтобы вывести список победителей\n");
+		printf("\n\nНажмите:\n 1 - чтобы показать таблицу\n 2 - чтобы вывести сортировку по дистанции и стилю\n 3 - чтобы очистить консоль\n 4 - чтобы выйти из консоли\n");
 		scanf_s("%d", &k);
 		switch (k) {
 		case 1: menю(consts, arr, names);
@@ -58,36 +57,43 @@ int main()
 			scanf_s("%lf %lf", &metr, &style);
 			poisk(consts, arr, names, metr, style);
 			break;
+		case 3: printf("Нажмите 3, чтобы очистить консоль\n");
+			system("cls");
+			break;
+		case 4: printf("Нажмите 4, чтобы выйти из консоли\n");
+			exit(0);
+			break;
 		default:printf("неизвестная команда\n");
 			break;
+
 		}
 	}
 }
-double rot(int k, float consts[5][2], double arr[5][5], int i) {
+double rot(int k, float consts[5][2], double arr[5][4], int i) {
 	if (k == 1.0) {
 		if (arr[i][0] == 50) {
-			arr[i][4] = 1000 * pow((consts[0][0] / arr[i][1]), 3.0);
+			arr[i][3] = 1000 * pow((consts[0][0] / arr[i][1]), 3.0);
 
 
 		}
-		else arr[i][4] = 1000 * pow((consts[1][0] / arr[i][1]), 3.0);
+		else arr[i][3] = 1000 * pow((consts[1][0] / arr[i][1]), 3.0);
 	}
 	if (k == 2.0) {
-		arr[i][4] = 1000 * pow((consts[2][0] / arr[i][1]), 3.0);
+		arr[i][3] = 1000 * pow((consts[2][0] / arr[i][1]), 3.0);
 	}
 	if (k == 3.0) {
-		arr[i][4] = 1000 * pow((consts[3][0] / arr[i][1]), 3.0);
+		arr[i][3] = 1000 * pow((consts[3][0] / arr[i][1]), 3.0);
 	}
 	if (k == 4.0) {
-		arr[i][4] = 1000 * pow((consts[4][0] / arr[i][1]), 3.0);
+		arr[i][3] = 1000 * pow((consts[2][0] / arr[i][1]), 3.0);
 	}
 
 
 }
 
-void menю(float consts [5][2], double arr[5][5], char names[][30]) {
+void menю(float consts [5][2], double arr[5][4], char names[][30]) {
 	setlocale(LC_ALL, "RUS");
-	printf("|Спортсмен | Дистанция, м | Время, с | Стиль | Кол-во заплывов|  Очки |");
+	printf("|Спортсмен | Дистанция, м | Время, с |  Стиль |   Очки   |");
 	double k;
 	for (int i = 0; i < 5; i++) {
 		k = arr[i][2];
@@ -96,17 +102,17 @@ void menю(float consts [5][2], double arr[5][5], char names[][30]) {
 	}
 	for (int i = 0; i < 5; i++) {
 		printf("\n\n");
-		for (int j = 0; j < 5; j++) {
+		for (int j = 0; j < 4; j++) {
 			if (j == 0) {
-				printf("%9s      %4.1f       ", names[i], arr[i][j]);
+				printf("%9s     %5.1f       ", names[i], arr[i][j]);
 			}
-			else { printf("  %4.1f      ", arr[i][j]); }
+			else { printf(" %5.1f      ", arr[i][j]); }
 		}
 		
 	}
 
 }
-void poisk(float consts[5][2], double arr[5][5], char names[][30], double metr, double style)
+void poisk(float consts[5][2], double arr[5][4], char names[][30], double metr, double style)
 {
 	int k = 0;
 	for (int i = 0; i < 5; i++) {
